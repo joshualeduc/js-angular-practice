8e40c702c7dab2dade106cd7d9dc8089e84fe28d
Joshua LeDuc <joshualeduc@gmail.com>
2017-04-25 14:02:49 -0600

    using http service

--- Angular2-GettingStarted/APM - Start/app/products/product-list.component.ts
+++ Angular2-GettingStarted/APM - Start/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string;
+  errorMessage: string;
   products: IProduct[] = [];
 
   constructor(private _productService: ProductService) { //this is where we define the injected service
@@ -25,7 +26,9 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.products = this._productService.getProducts();
+      this._productService.getProducts()
+        .subscribe(products => this.products = products,
+                   error => this.errorMessage = <any>error);
   }
 
   onRatingClicked(message: string): void {
