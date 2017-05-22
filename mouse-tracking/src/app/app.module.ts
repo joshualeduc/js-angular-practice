28e2351fc937448f91d4070c6c3babc7e7e21f65
Joshua LeDuc <joshualeduc@gmail.com>
2017-05-22 09:20:51 -0600

    npm packages

--- mouse-tracking/src/app/app.module.ts
+++ mouse-tracking/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MdCardModule, MdTooltipModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { } from 'dashjs';
 
 import { AppComponent } from './app.component';
 
