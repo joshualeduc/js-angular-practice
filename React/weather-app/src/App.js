be02e197e77f6fa468dba4718518e8f1fffcfcff
Josh LeDuc <joshualeduc@gmail.com>
2018-08-21 11:07:09 -0600

    starting base of app

--- React/weather-app/src/App.js
+++ React/weather-app/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Results from './components/Results'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <BrowserRouter>
+          <Home />
+          <Results />
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default App
