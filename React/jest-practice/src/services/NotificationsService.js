679197f032eb87bbec5fda929ceaaab163db55c8
Josh LeDuc <joshualeduc@outlook.com>
2019-08-04 19:06:55 -0400

    remove react

--- React/jest-practice/src/services/NotificationsService.js
+++
@@ -1,8 +0,0 @@
-import { delay } from 'redux-saga'
-export default {
-  async GetNotifications() {
-    console.warn('Real Notification Service! Really Contacting APIS, I promise!')
-    await(1000)
-    return { count: 42 };
-  }
-}
